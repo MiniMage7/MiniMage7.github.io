2df7fc89179b4f65c4b1fc903ce1d02af7705897
Winter <77jamesthomas@gmail.com>
2023-11-21 11:56:21 -0600

    Website Files
    Copied from Match3Solver

---
+++ solve.js
@@ -0,0 +1,324 @@
+// Javascript intepretation of the Match 3 Solver
+// Only usable through the website
+
+// Arrays used for solving
+const puzzleBoard = [];
+const movesToSolve = [];
+
+// Where output is written out to
+const outputBox = document.getElementById("output");
+
+// Function called from the website button to start the solve process
+function startSolve() {
+    outputBox.textContent = "solving";
+    setUpBoard();
+    solve();
+    // If this is reached, there was no solution found
+    outputBox.textContent = "There is no solution.";
+}
+
+// Sets the puzzleBoard and movesToSolve arrays up pre-solve
+function setUpBoard() {
+    // Empty the board if it isn't already
+    puzzleBoard.length = 0;
+    // Empty the solution moves if it isn't already
+    movesToSolve.length = 0;
+
+    // Get all the tiles
+    let tiles = tileContainer.getElementsByClassName("tile");
+
+    // For each row in the grid
+    for (let y = 0; y < height; y++) {
+        // For each column in the grid
+        const tempRowOfTiles = [];
+        for (let x = 0; x < width; x++) {
+            // Add that tile's c value to the row of tiles
+            const tile = tiles[y * height + x];
+            let cNumber = Number(getCNumber(tile));
+            tempRowOfTiles.push(cNumber);
+        }
+        // Add the row of tiles to the puzzleBoard
+        puzzleBoard.push(tempRowOfTiles);
+    }
+}
+
+// Recursive function that solves the puzzle
+function solve() {
+    checkForWin();
+
+    // For each row in the grid
+    for (let y = 0; y < height; y++) {
+        // For each column in the grid
+        for (let x = 0; x < width; x++) {
+            // If the tile is movable
+            if (puzzleBoard[y][x] > 0) {
+                // Check if the piece can be moved up or right
+                // There is no need to check down or left as those would be
+                // another piece's up or right respectively
+
+                if (checkValidMove(y, x, y - 1, x)) { // Up
+                    executeMove(y, x, y - 1, x);
+                }
+                if (checkValidMove(y, x, y, x + 1)) { // Right
+                    executeMove(y, x, y, x + 1);
+                }
+            }
+        }
+    }
+}
+
+// Checks if a move is a valid move
+// Input coordinates y1, x1 to be swapped with y2, x2
+function checkValidMove(y1, x1, y2, x2) {
+    // If the move is out of bounds
+    if (y2 < 0 || x2 >= width) {
+        return false;
+    }
+
+    // If the move is swapping with air or a blocker
+    if (puzzleBoard[y2][x2] < 1) {
+        return false;
+    }
+
+    // Swap the 2 spots on the puzzle board
+    let tempValue = puzzleBoard[y1][x1];
+    puzzleBoard[y1][x1] = puzzleBoard[y2][x2];
+    puzzleBoard[y2][x2] = tempValue;
+
+    // Check if the move results in any blocks being removed
+    let isMoveValid = (checkIfBlocksRemoved(y1, x1) || checkIfBlocksRemoved(y2, x2));
+
+    // Swap the pieces back
+    tempValue = puzzleBoard[y1][x1];
+    puzzleBoard[y1][x1] = puzzleBoard[y2][x2];
+    puzzleBoard[y2][x2] = tempValue;
+
+    return isMoveValid;
+}
+
+// Takes an x and y coordinate of the puzzle board
+// Checks if that piece should be removed
+function checkIfBlocksRemoved(y, x) {
+    // If it matches with the 2 blocks above it
+    if (y - 2 >= 0) {
+        if (puzzleBoard[y - 2][x] == puzzleBoard[y - 1][x] && puzzleBoard[y - 1][x] == puzzleBoard[y][x]) {
+            return true;
+        }
+    }
+    // If it matches with 1 block above it and 1 below it
+    if (y - 1 >= 0 && y + 1 < height) {
+        if (puzzleBoard[y - 1][x] == puzzleBoard[y][x] && puzzleBoard[y][x] == puzzleBoard[y + 1][x]) {
+            return true;
+        }
+    }
+    // If it matches with the 2 blocks below it
+    if (y + 2 < height) {
+        if (puzzleBoard[y][x] == puzzleBoard[y + 1][x] && puzzleBoard[y + 1][x] == puzzleBoard[y + 2][x]) {
+            return true;
+        }
+    }
+
+    // If it matches with the 2 blocks to the left of it
+    if (x - 2 >= 0) {
+        if (puzzleBoard[y][x - 2] == puzzleBoard[y][x - 1] && puzzleBoard[y][x - 1] == puzzleBoard[y][x]) {
+            return true;
+        }
+    }
+    // If it matches with the block to the left and to the right of it
+    if (x - 1 >= 0 && x + 1 < width) {
+        if (puzzleBoard[y][x - 1] == puzzleBoard[y][x] && puzzleBoard[y][x] == puzzleBoard[y][x + 1]) {
+            return true;
+        }
+    }
+    // If it matches with the 2 blocks to the right of it
+    if (x + 2 < width) {
+        if (puzzleBoard[y][x] == puzzleBoard[y][x + 1] && puzzleBoard[y][x + 1] == puzzleBoard[y][x + 2]) {
+            return true;
+        }
+    }
+
+    // If none of the moves resulted in blocks being removed
+    return false;
+}
+
+// Executes a given move on the board
+// Input coordinates y1, x1 to be swapped with y2, x2
+function executeMove(y1, x1, y2, x2) {
+    // Add the move to the move list
+    movesToSolve.push([`${y1}, ${x1}`, `${y2}, ${x2}`]);
+    // Save the current board state
+    const oldBoardState = JSON.parse(JSON.stringify(puzzleBoard));
+
+    // Execute the move and recalculate the new puzzle board
+    tempValue = puzzleBoard[y1][x1];
+    puzzleBoard[y1][x1] = puzzleBoard[y2][x2];
+    puzzleBoard[y2][x2] = tempValue;
+    recalculateBoard();
+
+    // Attempt to do the next move
+    solve();
+
+    // If this line was reached, the move was inccorect, so revert to the old board state
+    puzzleBoard.length = 0;
+    for (let index = 0; index < height; index++) {
+        puzzleBoard.push(oldBoardState[index]);
+    }
+    // And remove the move from the move list
+    movesToSolve.pop()
+}
+
+// Check if there are any pieces that need to be removed and removes them
+// Then rearranges the board to account for pieces falling
+// Recursively calls itself until no pieces move
+function recalculateBoard() {
+    // Get what blocks need to be removed
+    const blocksToRemove = checkWhatBlocksToRemove();
+
+    let isBlocksToRemove = false;
+    // For each row in the grid
+    for (let y = 0; y < height; y++) {
+        // For each column in the grid
+        for (let x = 0; x < width; x++) {
+            // If the tile is to be removed
+            if (blocksToRemove[y][x] == 1) {
+                isBlocksToRemove = true;
+            }
+        }
+    }
+
+    // If there are blocks to remove
+    if (isBlocksToRemove){
+        // Remove the blocks
+        removeGivenBlocks(blocksToRemove);
+        // Make all the blocks fall down
+        calculateGravity();
+        // Restart this process
+        recalculateBoard();
+    }
+}
+
+// Iterates over the whole puzzle board and returns what blocks need to be removed
+// Return: An 2d array of 0s and 1s where 1s represent the positions where blocks need to be removed
+function checkWhatBlocksToRemove() {
+    const blocksToRemove = [];
+
+    // Fill blockToRemove with 0s
+    // For each row in the grid
+    for (let y = 0; y < height; y++) {
+        // For each column in the grid
+        const tempRowOfTiles = [];
+        for (let x = 0; x < width; x++) {
+            tempRowOfTiles.push(0);
+        }
+        blocksToRemove.push(tempRowOfTiles);
+    }
+
+    // For each row in the grid
+    for (let y = 0; y < height; y++) {
+        // For each column in the grid
+        for (let x = 0; x < width; x++) {
+            // If the piece is a removable piece
+            if (puzzleBoard[y][x] > 0) {
+                // We only have to check for these 2 because all the other circumstances will be checked
+                // in another piece's 2 above or 2 to the right
+
+                // Check if it can be matched with the 2 pieces above it
+                if (y - 2 >= 0) {
+                    if (puzzleBoard[y - 2][x] == puzzleBoard[y - 1][x] && puzzleBoard[y - 1][x] == puzzleBoard[y][x]) {
+                        // Mark the pieces to be removed
+                        blocksToRemove[y - 2][x] = 1;
+                        blocksToRemove[y - 1][x] = 1;
+                        blocksToRemove[y][x] = 1;
+                    }
+                }
+
+                // Check if it can be matched with the 2 pieces to the right of it
+                if (x + 2 < width) {
+                    if (puzzleBoard[y][x] == puzzleBoard[y][x + 1] && puzzleBoard[y][x + 1] == puzzleBoard[y][x + 2]) {
+                        // Mark the pieces to be removed
+                        blocksToRemove[y][x] = 1;
+                        blocksToRemove[y][x + 1] = 1;
+                        blocksToRemove[y][x + 2] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    return blocksToRemove;
+}
+
+// Takes an array of 1's and 0's
+// Removes all blocks from the puzzle board where the given array has a 1 in the same position
+// Used with checkWhatBlocksToRemove()
+function removeGivenBlocks(blocksToRemove) {
+    // For each row in the grid
+    for (let y = 0; y < height; y++) {
+        // For each column in the grid
+        for (let x = 0; x < width; x++) {
+            // If the piece is marked to be removed
+            if (blocksToRemove[y][x] == 1) {
+                // Remove the piece
+                puzzleBoard[y][x] = 0;
+            }
+        }
+    }
+}
+
+// Makes all blocks that need to fall down in the puzzle board fall down
+// Moves all blocks with air under them down 1
+// Recursively calls itself until no blocks move
+// Blockers (-1s) do not fall
+function calculateGravity() {
+    let didBlocksMove = false;
+
+    // For each row in the grid (bottom to top skipping bottom most row)
+    for (let y = height - 2; y >= 0; y--) {
+        // For each column in the grid
+        for (let x = 0; x < width; x++) {
+            // If the piece is effected by gravity
+            if (puzzleBoard[y][x] > 0) {
+                // If there is air below it
+                if (puzzleBoard[y + 1][x] == 0) {
+                    // Move it down to that block
+                    puzzleBoard[y + 1][x] = puzzleBoard[y][x];
+                    puzzleBoard[y][x] = 0;
+                    didBlocksMove = true;
+                }
+            }
+        }
+    }
+
+    // If any blocks moved, check if any more gravity is needed
+    if (didBlocksMove) {
+        calculateGravity();
+    }
+}
+
+// Checks if any values in the array are greater than 0
+// If there are none, the puzzle is solved and calls output solution
+function checkForWin() {
+    // For each row in the grid
+    for (let y = 0; y < height; y++) {
+        // For each column in the grid
+        for (let x = 0; x < width; x++) {
+            // If there is a tile, return
+            if (puzzleBoard[y][x] > 0) {
+                return;
+            }
+        }
+    }
+
+    // If this line is reached, the puzzle is solved
+    outputSolution();
+    throw new Error("This is not an error. This is just to stop the solving process on success.");
+}
+
+function outputSolution() {
+    let outputString = "The solution is:\n\n";
+    for (let index = 0; index < movesToSolve.length; index++) {
+        outputString += `Swap: ${movesToSolve[index][0]} with ${movesToSolve[index][1]}\n`
+    }
+    outputString += "\nClick the question mark in the top right for help on how to read the solution."
+    outputBox.textContent = outputString;
+}
---
+++ website.js
@@ -0,0 +1,145 @@
+// Initialize the grid
+const tileContainer = document.getElementById("tilecontainer");
+const gridSizeBoxes = document.getElementsByClassName("sizeinput");
+const widthBox = gridSizeBoxes[0].children[0];
+const heightBox = gridSizeBoxes[1].children[0];
+let width = 0;
+let height = 0;
+updateGridSize();
+
+// Give the input boxes event handlers
+widthBox.addEventListener("change", updateGridSize);
+heightBox.addEventListener("change", updateGridSize);
+
+// For determining if the mouse is being held down
+let isMouseDown = false;
+document.addEventListener("mousedown", mouseDown);
+document.addEventListener("mouseup", mouseUp);
+
+// Help button events
+const helpArea = document.getElementById("helparea");
+document.getElementById("helpdiamond").addEventListener("click", showHelpArea);
+helpArea.addEventListener("click", hideHelpArea);
+
+// Add clear button event
+document.getElementById("clear").addEventListener("click", clearGrid);
+
+// Add solve puzzle event
+document.getElementById("solve").addEventListener("click", solveGrid);
+
+// When the grid size boxes are update, adds or removes tiles as needed
+function updateGridSize() {
+  let oldWidth = width;
+  let oldHeight = height;
+
+  // Make sure the changed values are in acceptable ranges
+  const maxSize = 15;
+  if (widthBox.value > maxSize) {
+    widthBox.value = maxSize;
+  } else if (widthBox.value < 0) {
+    widthBox.value = 0; 
+  }
+  if (heightBox.value > maxSize) {
+    heightBox.value = maxSize;
+  } else if (heightBox.value < 0) {
+    heightBox.value = 0; 
+  }
+
+  width = Number(widthBox.value);
+  height = Number(heightBox.value);
+
+  // Resize grid
+  tileContainer.style.gridTemplateColumns = "repeat(" + String(width) + ", auto)";
+  tileContainer.style.gridTemplateRows = "repeat(" + String(height) + ", auto)";
+
+  // Add tiles
+  if (width * height > oldWidth * oldHeight) {
+    for (let i = 0; i < (width * height - oldWidth * oldHeight); i++) {
+      const tile = document.createElement("div");
+      tile.classList.add("tile", "c0");
+      tile.addEventListener("mousedown", cChange);
+      tile.addEventListener("mouseenter", mouseEnterTile)
+      tileContainer.appendChild(tile);
+    }
+  }
+  // Remove tiles
+  else {
+    for (let i = 0; i < (oldWidth * oldHeight - width * height); i++) {
+      tileContainer.removeChild(tileContainer.lastElementChild);
+    }
+  }
+}
+
+// Changed the class up or down 1 on user click
+function cChange(e) {
+  let cNumber = Number(getCNumber(e.target));
+  let newCNumber = cNumber;
+
+  // If shift is being held go down
+  if (e.shiftKey) {
+    newCNumber = cNumber - 1;
+    if (newCNumber < -1) {
+      newCNumber = 10;
+    }
+  } // Otherwise go up by 1
+  else {
+    newCNumber = cNumber + 1;
+    if (newCNumber > 10) {
+      newCNumber = -1;
+    }
+  }
+
+  e.target.classList.replace("c" + cNumber, "c" + newCNumber);
+}
+
+// Returns the value of the current class
+function getCNumber(tile) {
+  return tile.className.replace("tile", "").replace("c", "").trim();
+}
+
+// If the mouse is down on a tile enter, treat it as a click
+function mouseEnterTile(e) {
+  if (isMouseDown) {
+    cChange(e);
+  }
+}
+
+// Keeps track of if the mouse is being pressed
+function mouseDown(e) {
+  isMouseDown = true;
+}
+
+// Keeps track of if the mouse is being pressed
+function mouseUp(e) {
+  isMouseDown = false;
+}
+
+// Makes all the tiles white (c0)
+function clearGrid(e) {
+  let tiles = tileContainer.getElementsByClassName("tile");
+
+  // For each tile
+  for (let index = 0; index < tiles.length; index++) {
+    const tile = tiles[index];
+    // Get the tiles c number
+    let cNumber = Number(getCNumber(tile));
+    // Replace its c class with c0
+    tile.classList.replace("c" + cNumber, "c0");
+  }
+}
+
+function solveGrid(e) {
+  startSolve();
+}
+
+// Shows the help rules
+function showHelpArea(e) {
+  helpArea.style.display = "block";
+}
+
+// Hides the help area
+function hideHelpArea(e) {
+  if (e.target.id == "helparea") {
+    helpArea.style.display = "none";
+  }
+}
